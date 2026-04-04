ccc8104140095716e8bb5bd24ed0aa8af4482e05
Hetvaddoriya <hetvaddoriya07@gmail.com>
2026-04-04 23:41:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,11 +135,11 @@ except:
     if conflict:
         st.error("⚠️ Conflict detected! Choose another time.")
     else:
-    try:
-    link = create_event(title, start.isoformat(), end.isoformat())
-    st.success(f"✅ Event Created: {link}")
-     except:
-     st.error("❌ Cannot create event in cloud")
+        try:
+            link = create_event(title, start.isoformat(), end.isoformat())
+            st.success(f"✅ Event Created: {link}")
+        except:
+            st.error("❌ Cannot create event in cloud")
 
 # ---- VIEW EVENTS ----
 st.header("Upcoming Events")
